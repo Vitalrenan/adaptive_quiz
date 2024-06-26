de5a75a7fe73dde63de82bcff38536182082fd14
Renan Vital <renan_andrade_90@hotmail.com>
2024-06-26 11:31:18 -0300

    first commit

---
+++ testes.py
@@ -0,0 +1,234 @@
+import streamlit as st
+from utils import setupAula, cursos_dict, trata_alternativas, quiz_orquestrator, gera_sugestoes, guardrail_camada_final
+st.set_page_config(layout="wide")
+import os
+import json
+from langchain_openai import AzureChatOpenAI
+from datetime import datetime, timedelta
+from azure.storage.blob import BlobServiceClient, BlobSasPermissions, generate_blob_sas
+
+#azure blog setup
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
+AZURE_CONTAINER_NAME = os.getenv('AZURE_CONTAINER_NAME')
+def upload_and_generate_sas_token(file_path, blob_name):  
+    _, extensao = os.path.splitext(file_path)  
+    extensao = extensao.split('?')[0]  
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")  
+    unique_blob_name = f"{blob_name}_{timestamp}{extensao}"  
+    try:  
+        blob_service_client = BlobServiceClient(account_url=f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net",  
+                                                credential=AZURE_ACCOUNT_KEY)  
+        blob_container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)  
+        with open(file_path, "rb") as file:  
+            blob_client = blob_container_client.upload_blob(name=unique_blob_name, data=file, overwrite=True)
+        token_expiry = datetime.now() + timedelta(hours=120)  
+        sas_token = generate_blob_sas(  
+            account_name=AZURE_ACCOUNT_NAME,  
+            container_name=AZURE_CONTAINER_NAME,  
+            blob_name=unique_blob_name,  
+            account_key=AZURE_ACCOUNT_KEY,  
+            permission=BlobSasPermissions(read=True),  
+            expiry=token_expiry  
+        )  
+        return f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER_NAME}/{unique_blob_name}?{sas_token}"  
+  
+    except Exception as e:  
+        print(f"Erro ao fazer upload do arquivo PDF e gerar o token SAS: {str(e)}") 
+         
+
+#Setup inicial
+llm = AzureChatOpenAI(
+  openai_api_version = os.environ["AZURE_OPENAI_API_VERSION"],
+  azure_deployment = os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"]
+)
+
+st.session_state['data_hora']= datetime.now().strftime("%d/%m/%Y %H:%M")
+#render
+colA, colB = st.columns(spec=[0.7,0.3])
+with colA:
+    st.title("Quiz assistant - Adaptive Learning")
+with colB:
+    logo='logo_cogna.png'
+    st.image(logo)
+    
+    
+with st.sidebar:
+    st.image(logo)
+
+    cursos=cursos_dict()
+    user_name = st.text_input('Insira seu nome e confirme')
+    user_curso = st.selectbox("Curso", (cursos.keys()))
+    if user_curso:
+        user_materia = st.selectbox("Materia", cursos[user_curso])
+    questao_inicial = st.toggle("Questão inicial do quiz", value = True)
+    with st.form("my-form"):
+        sugestoes_embaixador = st.text_area(label='Comentários e sugestões',placeholder='Durante o teste, a IA respondeu...')
+        confirm_button = st.form_submit_button("Salva comentário")
+    
+    
+    send_log = st.button(label='Enviar resultado do teste', type ='primary')
+    if send_log:
+        st.markdown('Dados enviados!')
+        st.markdown(':rocket:')
+
+
+comando_titulo, comando_descricao, feedback, alternativas, conteudo_aula = setupAula(user_materia)
+col1, col2 = st.columns(spec=[0.6,0.4])
+with col1: 
+    st.markdown(comando_titulo)
+    st.markdown(comando_descricao)
+    alternativas = alternativas.replace('[','').replace(']','').replace("'",'"')
+    alternativas, alternativa_correta = trata_alternativas(alternativas)
+    st.radio('alternativas',alternativas)
+    st.markdown(f"Gabarito: {feedback}. Alternativa correta: {alternativas[alternativa_correta.index('True')]}.")
+
+with col2:
+    if 'st_messages' not in st.session_state:
+        st.session_state['st_messages'] = []
+        st.session_state['messages'] = []
+        st.session_state['sugestoes'] = ''
+        st.session_state['opcao_A_input'] = ''
+        st.session_state['opcao_B_input'] = ''
+        st.session_state['opcao_C_input'] = ''
+        st.session_state['historico_sugestoes']=[]
+    reset_chat = st.button('Resetar histórico',use_container_width=True)
+    if reset_chat:
+        st.session_state['st_messages'] = []
+        st.session_state['messages'] = [] 
+        st.session_state['sugestoes'] = ''
+        st.session_state['opcao_A_input'] = ''
+        st.session_state['opcao_B_input'] = ''
+        st.session_state['opcao_C_input'] = ''
+        st.session_state['historico_sugestoes']=[]
+        
+    #Interação inicial
+    if st.session_state['st_messages'] == []:
+        completion, messages = quiz_orquestrator(llm=llm,
+                                            input_aluno='',
+                                            messages=st.session_state['messages'],
+                                            user_name=user_name,
+                                            user_curso=user_curso,
+                                            questao_inicial=questao_inicial,
+                                            questao=f"Questao:{comando_titulo}.{comando_descricao}.",
+                                            conteudo_aula=conteudo_aula)
+        st.session_state['messages'] = messages
+        st.session_state['st_messages'].append({"role": "assistant", "content": completion}) 
+    
+
+    #Renderiza chat                
+    for n,message in enumerate(st.session_state['st_messages']):
+        with st.chat_message(message["role"]):
+                st.markdown(message["content"]) 
+
+        
+        if st.session_state['sugestoes']=='':
+            if n+1==len(st.session_state['st_messages']):
+                with st.chat_message('user'):
+                    st.session_state['sugestoes'] = gera_sugestoes(llm=llm, questao=f"Questao:{comando_titulo}.{comando_descricao}.",  messages=st.session_state['messages'])  
+                    st.session_state['historico_sugestoes'].append(st.session_state['sugestoes'])
+                    opcao_A=st.session_state['sugestoes'][1]
+                    opcao_B=st.session_state['sugestoes'][2]
+                    opcao_C=st.session_state['sugestoes'][3]
+                    st.markdown('Aqui vão algumas sugestões de interação:') 
+                    opcao_A_input = st.button(opcao_A,key=f'{opcao_A}_{n}')
+                    opcao_B_input = st.button(opcao_B,key=f'{opcao_B}_{n}')
+                    opcao_C_input = st.button(opcao_C,key=f'{opcao_C}_{n}')
+                    st.session_state['opcao_A_input']=opcao_A_input
+                    st.session_state['opcao_B_input']=opcao_B_input
+                    st.session_state['opcao_C_input']=opcao_C_input
+        else:
+             if n+1==len(st.session_state['st_messages']):
+                with st.chat_message('user'):
+                    opcao_A=st.session_state['sugestoes'][1]
+                    opcao_B=st.session_state['sugestoes'][2]
+                    opcao_C=st.session_state['sugestoes'][3]
+                    st.markdown('Aqui vão algumas sugestões de interação:') 
+                    opcao_A_input = st.button(opcao_A,key=f'{opcao_A}_{n}')
+                    opcao_B_input = st.button(opcao_B,key=f'{opcao_B}_{n}')
+                    opcao_C_input = st.button(opcao_C,key=f'{opcao_C}_{n}')
+                    st.session_state['opcao_A_input']=opcao_A_input
+                    st.session_state['opcao_B_input']=opcao_B_input
+                    st.session_state['opcao_C_input']=opcao_C_input
+                    
+    #teste botoes  
+    if st.session_state['opcao_A_input']:
+        completion, messages = quiz_orquestrator(llm=llm,
+                                    input_aluno=st.session_state['sugestoes'][1],
+                                    messages=st.session_state['messages'],
+                                    user_name=user_name,
+                                    user_curso=user_curso,
+                                    questao_inicial=questao_inicial,
+                                    questao=f"Questao:{comando_titulo}.{comando_descricao}.",
+                                    conteudo_aula=conteudo_aula)
+        st.session_state['messages'] = messages
+        st.session_state['st_messages'].extend([{"role": "user", "content": st.session_state['sugestoes'][1]},
+                                            {"role": "assistant", "content": completion}])
+        st.session_state['sugestoes'] = gera_sugestoes(llm=llm, questao=f"Questao:{comando_titulo}.{comando_descricao}.",  messages=st.session_state['messages'])  
+        st.session_state['historico_sugestoes'].append(st.session_state['sugestoes'])
+        st.rerun()     
+    if st.session_state['opcao_B_input']:
+        completion, messages = quiz_orquestrator(llm=llm,
+                                    input_aluno=st.session_state['sugestoes'][2],
+                                    messages=st.session_state['messages'],
+                                    user_name=user_name,
+                                    user_curso=user_curso,
+                                    questao_inicial=questao_inicial,
+                                    questao=f"Questao:{comando_titulo}.{comando_descricao}.",
+                                    conteudo_aula=conteudo_aula)
+        st.session_state['messages'] = messages
+        st.session_state['st_messages'].extend([{"role": "user", "content": st.session_state['sugestoes'][2]},
+                                            {"role": "assistant", "content": completion}])
+        st.session_state['sugestoes'] = gera_sugestoes(llm=llm, questao=f"Questao:{comando_titulo}.{comando_descricao}.",  messages=st.session_state['messages'])  
+        st.session_state['historico_sugestoes'].append(st.session_state['sugestoes'])
+        st.rerun()    
+    if st.session_state['opcao_C_input']:
+        completion, messages = quiz_orquestrator(llm=llm,
+                                    input_aluno=st.session_state['sugestoes'][3],
+                                    messages=st.session_state['messages'],
+                                    user_name=user_name,
+                                    user_curso=user_curso,
+                                    questao_inicial=questao_inicial,
+                                    questao=f"Questao:{comando_titulo}.{comando_descricao}.",
+                                    conteudo_aula=conteudo_aula)
+        st.session_state['messages'] = messages
+        st.session_state['st_messages'].extend([{"role": "user", "content": st.session_state['sugestoes'][3]},
+                                            {"role": "assistant", "content": completion}])
+        st.session_state['sugestoes'] = gera_sugestoes(llm=llm, questao=f"Questao:{comando_titulo}.{comando_descricao}.",  messages=st.session_state['messages'])  
+        st.session_state['historico_sugestoes'].append(st.session_state['sugestoes'])
+        st.rerun()  
+            
+        #Monta interação de chat pós introdução
+    if resposta_aluno := st.chat_input("Ou escreva a sua dúvida aqui"):
+        completion, messages = quiz_orquestrator(llm=llm,
+                                            input_aluno=resposta_aluno,
+                                            messages=st.session_state['messages'],
+                                            user_name=user_name,
+                                            user_curso=user_curso,
+                                            questao_inicial=questao_inicial,
+                                            questao=f"Questao:{comando_titulo}.{comando_descricao}.",
+                                            conteudo_aula=conteudo_aula)
+        completion = guardrail_camada_final(llm, f"Questao:{comando_titulo}.{comando_descricao}.", completion)
+        st.session_state['messages'] = messages
+        st.session_state['st_messages'].extend([{"role": "user", "content": resposta_aluno},
+                                                {"role": "assistant", "content": completion}])
+        st.rerun()  
+        
+
+if send_log:
+    log = {
+            'data_teste':[st.session_state['data_hora']],
+            'nome_usuario':[user_name],
+            'nome_curso':[user_curso],
+            'sugestoes_embaixador':[sugestoes_embaixador],
+            'questao':[f"Questao:{comando_titulo}.{comando_descricao}."],
+            'gabarito':[f"Gabarito: {feedback}. Alternativa correta: {alternativas[alternativa_correta.index('True')]}."],
+            'historico':[st.session_state['messages']],
+            'historico_sugestoes':[st.session_state['historico_sugestoes']]
+        }
+    with open("adaptive-teste-zero.json", "w") as outfile:
+        json.dump(log, outfile, indent=4, sort_keys=False)
+    upload_success = upload_and_generate_sas_token('.//adaptive-teste-zero.json','adaptive-teste-zero')
+    st.write(upload_success)
+    
+        
---
+++ utils/adaptive.py
@@ -0,0 +1,234 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
+
+# from langchain.chains import SequentialChain
+# from langchain.chains import LLMChain
+# from langchain.prompts import ChatPromptTemplate
+# from langchain.chains import ConversationChain
+# from langchain.memory import ConversationSummaryBufferMemory
+# from langchain_openai import AzureChatOpenAI
+# from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+import pandas as pd
+import streamlit as st
+import re
+
+#função setup conteudo de aula e questão quiz
+def get_df(db, collection_name,colunas):
+    print(f"Coletando base {collection_name}\n")
+    lista_rows=[]
+    collection = db[collection_name]
+    query = collection.find()
+    for x in query:
+        filtered_row = {key: x.get(key) for key in colunas}
+        lista_rows.append(filtered_row)
+    df=pd.DataFrame(lista_rows)
+    return df
+
+def cursos_dict():
+    cursos={
+        'Pedagogia':['PRÁTICAS PEDAGÓGICAS: IDENTIDADE DOCENTE​','PENSAMENTO CIENTÍFICO','FUNCIONAMENTO DA EDUCAÇÃO BRASILEIRA E POLÍTICAS PÚBLICAS​','FUNDAMENTOS DA EDUCAÇÃO​','PSICOLOGIA DA EDUCAÇÃO E DA APRENDIZAGEM'],
+        'Administração':['MATEMÁTICA FINANCEIRA','LEGISLAÇÃO EMPRESARIAL APLICADA','ANÁLISE DE CUSTOS​','MÉTODOS QUANTITATIVOS​','OPTATIVA I (EDUCAÇÃO AMBIENTAL;GESTÃO DO CONHECIMENTO)​'],
+        'Gestão em RH':['MATEMÁTICA FINANCEIRA','LEGISLAÇÃO EMPRESARIAL APLICADA','GESTÃO DE PESSOAS​','MÉTODOS QUANTITATIVOS​','OPTATIVA  II (AVALIAÇÃO DE PERFORMANCE; COMUNICAÇÃO E EDUCAÇÃO CORPORATIVA; LIBRAS - LÍNGUA BRASILEIRA DE SINAIS)​','PROJETO INTEGRADO INOVAÇÃO - GESTÃO​'],
+        'Desenvolvimento de Sistemas':['ENGENHARIA DE SOFTWARE​','LINGUAGEM DE PROGRAMAÇÃO​','LÓGICA E MATEMÁTICA COMPUTACIONAL','ALGORITMOS E PROGRAMAÇÃO ESTRUTURADA​','ANÁLISE E MODELAGEM DE SISTEMAS​','PROJETO INTEGRADO INOVAÇÃO - ANÁLISE E DESENVOLVIMENTO DE SISTEMAS​'],
+        'Serviço Social':['INTRODUÇÃO À FILOSOFIA​','ESTATÍSTICA E INDICADORES SOCIAIS​','ADMINISTRAÇÃO E PLANEJAMENTO DE SERVIÇO SOCIAL​','LEGISLAÇÃO SOCIAL E DIREITOS HUMANOS​','ECONOMIA POLÍTICA​'],
+        'Perícia Criminal':['FUNDAMENTOS HISTÓRICOS E INTRODUÇÃO AO ESTUDO DO DIREITO','FUNDAMENTOS DE INVESTIGAÇÃO E CRIMINALÍSTICA​','TEORIA GERAL DO PROCESSO​','EXPANSÃO DA CRIMINALIDADE​','TEORIA GERAL DO DIREITO CONSTITUCIONAL','PROJETO INTEGRADO INOVAÇÃO - INVESTIGAÇÃO E PERÍCIA CRIMINAL​'],
+    }
+    return cursos
+
+def from_html_to_text(tag):
+    tag=tag.encode("utf-8")
+    soup = BeautifulSoup(tag, features="html.parser")
+    for script in soup(["script", "style"]):
+        script.extract()   
+    text = soup.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+def trata_alternativas(texto):
+    itens = re.findall(r"\{(.*?)\}", texto)
+    alternativas=[]
+    correcoes=[]
+    for item in itens:
+        alternativa=item.split('",')[0].split('"text": ')[1][1:]
+        alternativas.append(alternativa)
+        
+        correcao=item.split('"isRightAnswer": ')[1].split(',')[0]
+        correcoes.append(correcao)        
+    return alternativas, correcoes
+
+@st.cache_data 
+def setupAula(materia):
+    try:
+        server=os.environ["SERVER_TYPE"]
+        user_alx=os.environ["MONGO_ALEXANDRIA_USERNAME"]
+        pwd_alx=os.environ["MONGO_ALEXANDRIA_PASSWORD"]
+        port_alx=os.environ["MONGO_ALEXANDRIA_HOST"]
+        clt_alx="alexandria-kroton"
+        str_conn = f"{server}://{user_alx}:{pwd_alx}@{port_alx}"
+        client = MongoClient(str_conn)
+        db = client[clt_alx]
+    except:
+        st.error(body='Erro ao extrair questões do alexandria',icon='🚨')
+        return ''
+    
+    #requisições
+    df_disciplinas = get_df(db, 'disciplines', ['title','learningUnits'])
+    LearningUnit =df_disciplinas[df_disciplinas.title.isin([materia])]['learningUnits'].to_list()[0][0]
+    df_learningunits=get_df(db,'learningunits',['_id','classes'])
+    aula = df_learningunits[df_learningunits._id.isin([LearningUnit])]['classes'].to_list()[0][0]
+    df_classes=get_df(db,'classes',['_id','blocks'])
+    blocks = df_classes[df_classes._id.isin([aula])]['blocks'].to_list()[0]
+    df_blocks=get_df(db,'blocks',['_id','questions','content'])
+    questions = df_blocks[df_blocks._id.isin(blocks)]['questions'].to_list()[-1]
+    
+    conteudo_aula = df_blocks[df_blocks._id.isin(blocks)]['content'].to_list()
+    conteudo_aula_tratado=[]
+    for i in conteudo_aula:
+        if i!=None:
+            conteudo_aula_tratado.append(from_html_to_text(i))
+    conteudo_aula=(" ".join(conteudo_aula_tratado))
+    
+    df_questions=get_df(db,'questions',['_id','type','level','alternatives','description','feedback','title'])
+    df_questions = df_questions[df_questions._id.isin(questions)]
+    for i in df_questions.columns:
+        df_questions[i]=df_questions[i].astype(str)
+    df_questions = df_questions[df_questions.type.isin(['multiple-choice','complex-multiple-choice'])].sample()
+    comando_titulo= from_html_to_text(df_questions['title'].item())
+    comando_descricao=from_html_to_text(df_questions['description'].item())
+    feedback=from_html_to_text(df_questions['feedback'].item())
+    alternativas=from_html_to_text(df_questions['alternatives'].item())
+    return comando_titulo, comando_descricao, feedback, alternativas, conteudo_aula
+
+
+
+# ####################################
+# ####### Funções de chat ############
+# ####################################
+def chat_memory(questao, conteudo_aula):
+    #memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=2000)
+    #questao=questao.encode("utf-8")
+    #conteudo_aula=conteudo_aula.encode("utf-8")
+    embeddings = AzureOpenAIEmbeddings(
+        azure_endpoint='https://openai-dados-lab-poc.openai.azure.com/',
+        api_key=os.environ["AZURE_OPENAI_API_KEY"]
+    )  
+    conteudo_relacionado = get_aula_rag(conteudo_aula, questao, embeddings)
+    messages=\
+        [["system",f"""Act as a very skilled university professor in assisting students on academic quizes.
+        You often tell the necessary basic concepts to solve it and then ask if the student understood them or if wants to proceed to the resolution.
+        The AI is assisting the student on this question: {questao}.
+        The AI completion must use the following text as reference: {conteudo_relacionado}.
+        Important: You must answer this onging conversation in brazillian portuguese, very briefly, concise and kindly."""]]
+    return messages
+
+#rag
+def get_aula_rag(conteudo_aula, questao, embeddings):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len,
+        is_separator_regex=False)
+    docs = splitter.create_documents([conteudo_aula])
+    db = Chroma.from_documents(docs, embeddings)
+    retriever = db.as_retriever()
+    best_docs=retriever.invoke(questao, search_kwargs={"k": 1})
+    conteudo_relacionado = "".join([best_docs[i].page_content for i in range(len(best_docs))])
+    return conteudo_relacionado
+
+# funções de interação
+def interacao_inicial(user_name, user_curso, llm, questao_inicial, messages):
+    if questao_inicial == True:
+        template_inicial = \
+        f"""Context: Act as a kind academic professor assisting your student {user_name}, from {user_curso} course.
+        Action: Congratulate {user_name} for another step in {user_curso} carreer. Then just inform that you can help with
+        the quiz question.
+        Important: Answer briefly, concise and kindly in brazillian portuguese. Do not write more than one paragraph."""
+    else:
+        template_inicial = \
+        f"""Context: Act as a kind academic professor assisting your student {user_name}.
+        Action: Now inform you are moving to the next question. Then, tell your student you can help again.
+        Important: Answer briefly, concise and kindly in brazillian portuguese. Do not write more than one paragraph."""
+    completion_inicial = llm.invoke(template_inicial)
+    completion_inicial = completion_inicial.content
+    completion_inicial = completion_inicial.replace('Tutor: ','').replace('Professor: ','')
+    messages.append(["assistant",str(completion_inicial)])
+    return completion_inicial, messages
+
+def conversa(input_aluno, messages, llm):   
+    input_aluno = f"""{input_aluno}. Atention! Do not solve the question. Instead of solving it,
+    please briefly explain me the necessary reasoning to solve it. Then, tell me to try to apply the reasoning.
+    Do never offer to calculate too, tell me to try instead."""
+    messages.append(["user",str(input_aluno)])
+    response=llm.invoke(messages)
+    response = response.content
+    if response.startswith('Olá!'):
+        response = response.replace('Olá! ','')
+    messages.append(["assistant", response])
+    return response, messages
+
+def quiz_orquestrator(llm, input_aluno, messages, user_name, user_curso, questao_inicial, questao, conteudo_aula):
+    if messages==[]:
+        messages = chat_memory(questao, conteudo_aula)
+        completion, messages = interacao_inicial(user_name, user_curso, llm, questao_inicial, messages)
+        return completion, messages 
+    else:
+        completion , messages = conversa(input_aluno, messages, llm)
+    return completion, messages
+
+def gera_sugestoes(llm, questao, messages):
+    conteudo_relacionado = messages[0][1]
+    conteudo_relacionado = conteudo_relacionado.split('text as reference: ')[1].split('Important:')[0]
+    ultima_conversa = messages[-1][1]
+    prompt=f"""Context:Act as a university professor assisting your student to develop a reasoning about\
+            the question below: {questao};
+            In order to help, I will provide you with some related content. Here is a brief summary of the content:\
+            {conteudo_relacionado};
+            Now, you should provide to your student three possible questions that could be asked about the content above.
+            System: Ai must always answer in brazillian portuguese, in a very concise and kind way. Sometimes you can be funny using ASCII emojis.
+            System: Ai completion should be in the following format:\
+            -> Here goes the AI first generated question.\
+            -> Here goes the AI second generated question.\
+            -> Here goes the AI third generated question.\
+            
+            Here goes an example for you to follow:
+            -> O que é a Vontade para Schopenhauer?
+            -> Como Schopenhauer vê o papel da arte em relação à Vontade?
+            -> Quais são as principais diferenças entre a estética de Schopenhauer e a de Kant?
+            
+            System: AI must never suggest a question about if an alternative is right. The suggestions should guide\
+            the student to understand the reasoning to solve the question instead. 
+            Important: You must change your suggestion if it looks like this: {ultima_conversa}.
+            """
+    sugestoes = llm.invoke(prompt)
+    sugestoes = sugestoes.content
+    sugestoes_list = sugestoes.split("->")
+    sugestoes_dict = {}
+    for n,i in enumerate(sugestoes_list[1:]):
+        sugestoes_dict[n+1] = i.replace('\n','')[1:]
+    return sugestoes_dict
+
+def guardrail_camada_final(llm, questao, completion):
+    prompt=f"""You are a text moderator checking if a text between triple 
+    backticks is infringing any of the following rules:
+    - The text must be written in Brazillian portuguese;
+    - The AI is not allowed to answer questions about billing or exams schedules;
+    - The AI must stick to the following quiz question context: {questao},
+    
+    Now, If the analyzed text infringed any of the above rules, here goes the actions
+    you must take to correct it:
+    
+    - If the text is written in any another language than brazillian portuguese, you must translate it to brazillian portuguese;
+    - If the text contains explanations about billing or exams schedules, you should rewrite to: 'Eu sou uma IA treinada apenas para ajudar com questões do quiz, infelizmente não posso ajudar com outro tipo de dúvida. Caso tenha alguma dúvida sobre o quiz, ficarei feliz em poder ajudar;
+    - If the text contains subjects too different from quiz context, you should rewrite to: 'Eu sou uma IA treinada apenas para ajudar com questões do quiz, infelizmente não posso ajudar com outro tipo de dúvida. Caso tenha alguma dúvida sobre o quiz, ficarei feliz em poder ajudar;
+    
+    Here goes the text that AI should moderate: ```{completion}```
+    Finally: If the text did not infringe any rule, AI should just replicate the original text.
+    """
+    final_guardrail = llm.invoke(prompt)
+    return  final_guardrail.content
